251e8e726d5e459b6db60391a3176bfbffc0854a
Laureen François <francois.laureen@gmail.com>
2017-12-12 14:50:15 +0100

    Header finished + add fonts + CSS header + js roll menu

---
+++ web/js/script.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function myFunction() {
+    console.log("myFunction");
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+};
